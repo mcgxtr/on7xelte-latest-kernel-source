c35de84214de8143c1f3e09e21120ba1b09f0351
Jack01 <62480469+mcgxtr@users.noreply.github.com>
2026-01-08 18:56:22 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
-
+# Değişkenler
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=o
+export CROSS_COMPILE=~/Downloads/Toolchains-main/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
+# Kernel Klasöründeki Hataları Otomatik Tamir Et (yylloc ve sdcardfs)
+sed -i 's/YYLTYPE yylloc;/extern YYLTYPE yylloc;/g' scripts/dtc/dtc-lexer.lex.c_shipped 2>/dev/null || true
+sed -i '/source "fs\/sdcardfs\/Kconfig"/d' fs/sdcardfs/Kconfig 2>/dev/null || true
+
+# Derleme
+make clean
 make exynos7870-on7xelteswa_defconfig
-make -j64
+make HOSTCFLAGS="-fcommon" -j$(nproc)
